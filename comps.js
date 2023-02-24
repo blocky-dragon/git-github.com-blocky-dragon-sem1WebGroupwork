402cd7fa99acec683b08d75324778d0179457296
cordlessGnu <ben.hoggenator@gmail.com>
2023-02-24 12:49:28 +0000

    Changes to comps javascript

--- comps.js
+++ comps.js
@@ -1,42 +1,70 @@
 //declare vars
 
-var rockButton = undefined;
-var blocButton = undefined;
+var dcTrophyButton = undefined;
+var scTrophyButton = undefined;
+var mTrophyButton = undefined;
+var ghTrophyButton = undefined;
 
 window.onload = function(){
 
 
 
-    //get the rock button
-    rockButton = document.getElementById('actualImg1');
-    rockButton.addEventListener('mouseover', rockHovered, false); 
-    rockButton.addEventListener('mouseleave', rockDeHovered, false); 
+    //get the dcTrophy button
+    dcTrophyButton = document.getElementById('actualImg1');
+    dcTrophyButton.addEventListener('mouseover', dcHovered, false); 
+    dcTrophyButton.addEventListener('mouseleave', dcDeHovered, false); 
 
+    //get the scTrophy button
+    scTrophyButton = document.getElementById('actualImg2');
+    scTrophyButton.addEventListener('mouseover', scHovered, false); 
+    scTrophyButton.addEventListener('mouseleave', scDeHovered, false)
 
+    //get the mTrophy button
+    mTrophyButton = document.getElementById('actualImg3');
+    mTrophyButton.addEventListener('mouseover', mHovered, false); 
+    mTrophyButton.addEventListener('mouseleave', mDeHovered, false)
 
-    //get the bloc button
-    blocButton = document.getElementById('actualImg2');
-    blocButton.addEventListener('mouseover', blocHovered, false); 
-    blocButton.addEventListener('mouseleave', blocDeHovered, false)
+    //get the ghTrophy button
+    ghTrophyButton = document.getElementById('actualImg4');
+    ghTrophyButton.addEventListener('mouseover', ghHovered, false); 
+    ghTrophyButton.addEventListener('mouseleave', ghDeHovered, false)
 
 
 }
 
 
-//rock
-function rockHovered(){
-    rockButton.src = "images/alienRockHidden.png";
+//dcTrophy
+function dcHovered(){
+    dcTrophyButton.src = "images/fadedTrophy1.png";
 }
 
-function rockDeHovered(){
-    rockButton.src = "images/alienRock.png";
+function dcDeHovered(){
+    dcTrophyButton.src = "images/trophy1.png";
 }
 
-//bloc
-function blocHovered(){
-    blocButton.src = "images/alienBlocHidden.png";
+//scTrophy
+function scHovered(){
+    scTrophyButton.src = "images/alienBlocHidden.png";
 }
 
-function blocDeHovered(){
-    blocButton.src = "images/alienBloc.png";
+function scDeHovered(){
+    scTrophyButton.src = "images/alienBloc.png";
+}
+
+//mTrophy
+function mHovered(){
+    mTrophyButton.src = "images/alienBlocHidden.png";
+}
+
+function mDeHovered(){
+    mTrophyButton.src = "images/alienBloc.png";
+}
+
+//ghTrophy
+function ghHovered(){
+    ghTrophyButton.src = "images/fadedTrophy4.png";
+}
+
+function ghDeHovered(){
+    ghTrophyButton.src = "images/Trophy4.png";
 }
